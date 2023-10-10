a2f550da05f8a3bfbcfa2d7f318a0696206d602c
Duong Minh Hoang <hoangdm.141196@gmail.com>
2023-10-10 10:41:51 +0700

    updating data

--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,7 @@ import Root, {
 } from "./routes/root";
 import ErrorPage from "./error-page";
 import Contact, { loader as contactLoader } from "./routes/contact";
+import EditContact, {loader as editContactLoader} from "./routes/edit";
 
 const router = createBrowserRouter([
   {
@@ -21,6 +22,11 @@ const router = createBrowserRouter([
         path: "contacts/:contactId",
         element: <Contact />,
         loader: contactLoader,
+      },
+      {
+        path: "contacts/:contactId/edit",
+        element: <EditContact />,
+        loader: editContactLoader,
       },
     ],
   },
